bccb4b7ed5f3229e09cdab766b87bcca2b59217d
JH <jiho4890@gmail.com>
2026-04-08 16:19:41 +0900

    chore: 프로젝트 초기 세팅
    - 패키지 생성
    - application.properties 세팅

---
+++ src/main/java/com/vanilalatte/scheduler/SchedulerSpartCh3Application.java
@@ -0,0 +1,13 @@
+package com.vanilalatte.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerSpartCh3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerSpartCh3Application.class, args);
+    }
+
+}
